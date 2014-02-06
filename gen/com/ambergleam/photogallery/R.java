0d1f9c0c6c45e1a0c9fe9ddec3ae71984e594545
matthew-compton <compton@bignerdranch.com>
2014-02-06 17:35:43 -0500

    Finished chapter 27

--- gen/com/ambergleam/photogallery/R.java
+++ gen/com/ambergleam/photogallery/R.java
@@ -21,15 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int brian_up_close=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int fragmentContainer=0x7f070000;
+        public static final int gallery_item_imageView=0x7f070002;
         public static final int gridView=0x7f070001;
     }
     public static final class layout {
         public static final int activity_fragment=0x7f030000;
         public static final int fragment_photo_gallery=0x7f030001;
+        public static final int gallery_item=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
--- src/com/ambergleam/photogallery/PhotoGalleryFragment.java
+++ src/com/ambergleam/photogallery/PhotoGalleryFragment.java
@@ -2,8 +2,10 @@ package com.ambergleam.photogallery;
 
 import java.util.ArrayList;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class PhotoGalleryFragment extends Fragment {
 
@@ -18,13 +21,27 @@ public class PhotoGalleryFragment extends Fragment {
 
 	GridView mGridView;
 	ArrayList<GalleryItem> mItems;
+	ThumbnailDownloader<ImageView> mThumbnailThread;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate");
+
 		setRetainInstance(true);
 		new FetchItemsTask().execute();
+
+		mThumbnailThread = new ThumbnailDownloader<ImageView>(new Handler());
+		mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>() {
+			public void onThumbnailDownloaded(ImageView imageView, Bitmap thumbnail) {
+				if (isVisible()) {
+					imageView.setImageBitmap(thumbnail);
+				}
+			}
+		});
+		mThumbnailThread.start();
+		mThumbnailThread.getLooper();
+		Log.i(TAG, "Background thread started");
 	}
 
 	@Override
@@ -41,10 +58,31 @@ public class PhotoGalleryFragment extends Fragment {
 		}
 
 		if (mItems != null) {
-			mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
+			mGridView.setAdapter(new GalleryItemAdapter(mItems));
 		} else {
 			mGridView.setAdapter(null);
 		}
+	}
+
+	private class GalleryItemAdapter extends ArrayAdapter<GalleryItem> {
+		public GalleryItemAdapter(ArrayList<GalleryItem> items) {
+			super(getActivity(), 0, items);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.gallery_item, parent, false);
+			}
+
+			ImageView imageView = (ImageView) convertView.findViewById(R.id.gallery_item_imageView);
+			imageView.setImageResource(R.drawable.brian_up_close);
+			GalleryItem item = getItem(position);
+			mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+
+			return convertView;
+		}
+
 	}
 
 	private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
@@ -60,6 +98,19 @@ public class PhotoGalleryFragment extends Fragment {
 			mItems = items;
 			setupAdapter();
 		}
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mThumbnailThread.quit();
+		Log.i(TAG, "Background thread destroyed");
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		mThumbnailThread.clearQueue();
 	}
 
 }
